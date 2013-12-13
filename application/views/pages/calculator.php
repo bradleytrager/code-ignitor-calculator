06d2423c8b34757b35dfb8440e61f716a1abde85
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2013-12-13 09:02:39 -0500

    refactored markup

--- application/views/pages/calculator.php
+++ application/views/pages/calculator.php
@@ -1,19 +1,29 @@
 
 <div class="col-sm-12">
 	<h1>Calculator</h1>
-
-	
-
-	<?php echo form_open('calculator', array('class'=> 'form-inline')) ?>
-		<input type="text"  class="col-sm-2"  name="operandA" placeholder="First Operand" value="<?php echo $operandA; ?>"/>
-		<input type="text"  class="col-sm-2" name="operator" placeholder="Operator (+, -, *, /)" value="<?php echo $operator; ?>"/>
-		<input type="text"  class="col-sm-2"  name="operandB" placeholder="Second Operand" value="<?php echo $operandB; ?>"/>
-		<input type="submit" class="btn btn-default" name="submit" value="Evaluate" />
-	</form>
-	<ul><?php echo validation_errors(); ?></ul>
-	
-
-	<?php if(isset($result)){ ?>
-	<h3 class="text-success">Result: <?php echo $result; ?></h3>
-	<?php } ?>
 </div>
+</div><!--end row-->
+<?php echo form_open('calculator', array('class'=> 'form-inline')) ?>
+<div class="row">
+	<div class="col-sm-4">
+		<input type="text"  class="form-control"  name="operandA" placeholder="First Operand" value="<?php echo $operandA; ?>"/>
+	</div>
+	<div class="col-sm-2">
+		<input type="text"  class="form-control" name="operator" placeholder="Operator (+, -, *, /)" value="<?php echo $operator; ?>"/>
+	</div>
+	<div class="col-sm-4">
+		<input type="text"  class="form-control"  name="operandB" placeholder="Second Operand" value="<?php echo $operandB; ?>"/>
+
+	</div>
+	<div class="col-sm-2">
+		<input type="submit" class="btn btn-default" name="submit" value="Evaluate" />
+	</div>
+</div>
+</form>
+
+<ul><?php echo validation_errors(); ?></ul>
+
+<?php if(isset($result)){ ?>
+<h3 class="text-success">Result: <?php echo $result; ?></h3>
+<?php } ?>
+
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,3 @@
-</div>
-</div>
-</body>
+			</div><!--end container-->
+	</body>
 </html>
